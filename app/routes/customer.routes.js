b6c1bad40e7d6ab13303af8f1cd1ed4d7b43c864
Federico Clinaz <federico.clinaz@web.de>
2021-09-19 16:19:53 +0200

    login/register controller

--- app/routes/customer.routes.js
+++ app/routes/customer.routes.js
@@ -1,5 +1,10 @@
+const {body} = require('express-validator');
+const {register} = require('../controllers/registerController');
+const {login} = require('../controllers/loginController');
+
 module.exports = app => {
   const customers = require("../controllers/customer.controller.js");
+
 
   // Create a new Customer
   app.post("/customers", customers.create);
@@ -18,4 +23,28 @@ module.exports = app => {
 
   // Create a new Customer
   app.delete("/customers", customers.deleteAll);
+  
+  app.post('/login',[
+		body('email',"Invalid email address")
+		.notEmpty()
+		.escape()
+		.trim().isEmail(),
+		body('password',"The Password must be of minimum 4 characters length").notEmpty().trim().isLength({ min: 4 }),
+	],login);
+  
+  
+  app.post('/register', [
+		body('name',"The name must be of minimum 3 characters length")
+		.notEmpty()
+		.escape()
+		.trim()
+		.isLength({ min: 3 }),
+		body('email',"Invalid email address")
+		.notEmpty()
+		.escape()
+		.trim().isEmail(),
+		body('password',"The Password must be of minimum 4 characters length").notEmpty().trim().isLength({ min: 4 }),
+	], register);
+  
+  
 };
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 // parse requests of content-type: application/json
 app.use(bodyParser.json());
 
@@ -10,10 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  console.log('Body: ', req.body);
 });
 
 require("./app/routes/customer.routes.js")(app);
+
 
 // set port, listen for requests
 app.listen(3000, () => {
